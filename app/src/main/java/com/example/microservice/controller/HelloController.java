1564dc0ef002faa548989849128e403fe4048050
Manuel Coppotelli <manuelcoppotelli@users.noreply.github.com>
2026-04-12 20:04:32 +0200

    improve response

--- app/src/main/java/com/example/microservice/controller/HelloController.java
+++ app/src/main/java/com/example/microservice/controller/HelloController.java
@@ -14,8 +14,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public Map<String, String> hello(@RequestParam(defaultValue = "World") String name) {
-        Map<String, String> response = new HashMap<>();
-        name = null;
+        Map<String, String> response = null;
         response.put("message", "Hello, " + name + "!");
         response.put("service", "sample-microservice");
         return response;
